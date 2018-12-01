2e9cc17511772862b6bb8ac369a81f3595f95dd8
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2018-12-01 12:15:14 -0800

    wip:

--- lib/index.ts
+++ lib/index.ts
@@ -4,27 +4,21 @@ import { google as googleAPIClient} from 'googleapis'
 import { split } from 'lodash/fp'
 import sheets from './helpers/spreadsheets'
 
-const buildClient = (GOOGLE_SERVICE_ACCOUNT_B64: string, SCOPES: string) => {
-  if (GOOGLE_SERVICE_ACCOUNT_B64 === undefined)
-    if (typeof process.env.GOOGLE_SERVICE_ACCOUNT_B64 !== 'string') {
-      throw Error('GOOGLE_SERVICE_ACCOUNT_B64 must be defined as environment variable or passed as parameter')
-    } else GOOGLE_SERVICE_ACCOUNT_B64 = process.env.GOOGLE_SERVICE_ACCOUNT_B64
+const buildClient = (googleServiceAccountB64: string, scopes: string) => {
+  const scopesSplit = split(',')(scopes)
 
-  const scopes = split(',')(SCOPES || process.env.SCOPES || 'https://www.googleapis.com/auth/spreadsheets')
-
-  const GOOGLE_KEYS = JSON.parse(
-    Buffer.from(GOOGLE_SERVICE_ACCOUNT_B64, 'base64').toString('utf8')
+  const googleKeys = JSON.parse(
+    Buffer.from(googleServiceAccountB64, 'base64').toString('utf8')
   )
 
-  // @ts-ignore: Load JWT or UserRefreshClient from GOOGLE_KEYS environmental variable. (they have incompatible types)
-  const client: JWT = auth.fromJSON(GOOGLE_KEYS)
-  client.scopes = scopes
+  const client = <JWT>auth.fromJSON(googleKeys)
+  client.scopes = scopesSplit
 
   return client
 }
 
-const authorize = async (GOOGLE_SERVICE_ACCOUNT_B64: string, SCOPES: string) => {
-  const client = buildClient(GOOGLE_SERVICE_ACCOUNT_B64, SCOPES)
+const authorize = async (googleServiceAccountB64: string, scopes: string) => {
+  const client = buildClient(googleServiceAccountB64, scopes)
   await client.authorize()
   googleAPIClient.options({ auth: client })
   return {
